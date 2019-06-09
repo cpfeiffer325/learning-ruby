b354e1e7ffc1304d3f67970eea3763578a7847b0
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-08 22:58:38 -0600

    exercises 11 to 13

---
+++ ex13.rb
@@ -0,0 +1,16 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+print "Your fourth variable is: "
+fourth = STDIN.gets.chomp
+print "Your fiftth variable is: "
+fifth = STDIN.gets.chomp
+
+
+
+
+
+
